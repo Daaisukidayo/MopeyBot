708f3589f17aab2342efd07d0373c3457f724d34
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-17 18:45:19 +0100

    fix: update pumpkin command to use new environment variable structure

--- commands/luck/pumpkin.js
+++ commands/luck/pumpkin.js
@@ -44,10 +44,10 @@ module.exports = [{
         ]]]]]]]]]]
     ]
 
-    $let[thum;$env[bb$get[i];thum]]
-    $let[MC;$randomNumber[$env[data;bb$get[i];mc;0];$env[data;bb$get[i];mc;1]]]
-    $let[desc;__$env[data;bb$get[i];desc]__ $env[data;bb$get[i];emoji]]
-    $let[clr;$env[data;bb$get[i];clr]]
+    $let[thum;$env[pk$get[i];thum]]
+    $let[MC;$randomNumber[$env[data;pk$get[i];mc;0];$env[data;pk$get[i];mc;1]]]
+    $let[desc;__$env[data;pk$get[i];desc]__ $env[data;pk$get[i];emoji]]
+    $let[clr;$env[data;pk$get[i];clr]]
     $if[$get[i]!=0; $let[f;$splitText[$sub[$get[i];1]]] ]
 
     $callFunction[sumMC;$get[MC]]
@@ -55,7 +55,7 @@ module.exports = [{
     $sendMessage[$channelID;
         $color[$get[clr]]
         $description[### $randomText[You were walking across the map and saw a $get[desc];You were wandering around the map and spotted a $get[desc];You stumbled upon $get[desc] while exploring;You were gliding across the map when you found a $get[desc];You discovered a $get[desc] while roaming the map;You were cruising through the wild and noticed a $get[desc];You found a $get[desc] lying on the map;You were sliding through the map when you saw a $get[desc];You noticed a $get[desc] on your path]!
-        ### $randomText[You held it for some time, goaled it and earned;You held it carefully, goaled it, and earned;You carried it for a while, scored it, and earned;You held onto it, delivered it to the hole, and earned;You protected it, goaled it, and collected;You kept it safe, scored it, and gained;You grabbed it, goaled it, and pocketed;You carried it to the hole and earned;You picked it up, delivered it to the hole, and earned;You kept it safe, goaled it, and scored;You held it carefully, goaled it, and earned] $separateNumber[$get[MC];.]$getGlobalVar[emoji]!]
+        ### $randomText[You held it for some time, goaled it and earned;You held it carefully, goaled it, and earned;You carried it for a while, scored it, and earned;You held onto it, delivered it to the hole, and earned;You protected it, goaled it, and collected;You kept it safe, scored it, and gained;You grapked it, goaled it, and pocketed;You carried it to the hole and earned;You picked it up, delivered it to the hole, and earned;You kept it safe, goaled it, and scored;You held it carefully, goaled it, and earned] $separateNumber[$get[MC];.]$getGlobalVar[emoji]!]
         $thumbnail[$get[thum]]
         $author[$userDisplayName • MUID: $getUserVar[MUID];$userAvatar]
 
