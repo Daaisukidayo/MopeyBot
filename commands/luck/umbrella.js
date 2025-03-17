755d3de772d92dd776e90bccff5ce26618454594
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-17 21:20:39 +0100

    bug fixes

--- commands/luck/umbrella.js
+++ commands/luck/umbrella.js
@@ -54,7 +54,7 @@ module.exports = ({
 
     $sendMessage[$channelID;
         $color[$get[clr]]
-        $description[### $randomText[You were walking across the ocean and saw $var[desc];You were wandering around the ocean and spotted $var[desc];You stumbled upon $var[desc] while exploring;You were gliding across the ocean when you found $var[desc];You discovered $var[desc] while roaming the ocean;You were cruising through the wild and noticed $var[desc];You found $var[desc] lying on the ocean;You were sliding through the ocean when you saw $var[desc];You noticed $var[desc] on your path]!
+        $description[### $randomText[You were walking across the ocean and saw $get[desc];You were wandering around the ocean and spotted $get[desc];You stumbled upon $get[desc] while exploring;You were gliding across the ocean when you found $get[desc];You discovered $get[desc] while roaming the ocean;You were cruising through the wild and noticed $get[desc];You found $get[desc] lying on the ocean;You were sliding through the ocean when you saw $get[desc];You noticed $get[desc] on your path]!
         ### $randomText[You held it for some time and earned;You held it carefully and earned;You carried it for a while and earned;You held onto it and earned;You protected it and collected;You kept it safe and gained;You grabbed it and pocketed;You held it for some time and earned;You picked it up and earned;You kept it safe and scored;You held it carefully and earned] $separateNumber[$get[MC];.]$getGlobalVar[emoji]!]
         $thumbnail[$get[thum]]
         $author[$userDisplayName • MUID: $getUserVar[MUID];$userAvatar]
