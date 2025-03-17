a54974d379aa24de66ceb0c08487f13ad9f3ab6c
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-17 19:03:13 +0100

    bug fixes

--- commands/dev/restart.js
+++ commands/dev/restart.js
@@ -5,8 +5,7 @@ module.exports = ({
 
     $onlyIf[$authorID==$botOwnerID]
     Shutting down the bot and turning it on in a few seconds.
-    $let[code;$escapeCode[if [[ -d .git \]\] && [[ 1 == "1" \]\]\; then git pull\; fi\; if [[ ! -z \${NODE_PACKAGES} \]\]\; then /usr/local/bin/npm install \${NODE_PACKAGES}\; fi\; if [[ ! -z \${UNNODE_PACKAGES} \]\]\; then /usr/local/bin/npm uninstall \${UNNODE_PACKAGES}\; fi\; if [ -f /home/container/package.json \]\; then /usr/local/bin/npm install\; fi\; if [[ ! -z "\${START_BASH_FILE}" \]\]\; then bash \${START_BASH_FILE}\; else /usr/local/bin/node /home/container/index.js\; fi\;]]
-    $setTimeout[$exec[$get[code]];10s]
+    $setTimeout[$exec[npm start];10s]
     $botDestroy
     `
 })
