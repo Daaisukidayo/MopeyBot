8f12175e2aafe41472d7cb8fc7187dfbde31b10c
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-18 18:44:51 +0100

    fix: refactor raretryMode command to use embed function for cleaner code

--- commands/luck/raretryMode.js
+++ commands/luck/raretryMode.js
@@ -2,34 +2,37 @@ module.exports = [{
   name: "raretrymode",
   aliases: ["rtm", "rtmode"],
   type: "messageCreate",
-  code: `
-    
-    $author[$userDisplayName • MUID: $getUserVar[MUID];$userAvatar]
-    $description[# Choose your raretry mode:]
-    $color[228b22]
-
-    $arrayLoad[modes;, ;inferno, default, medium, hard, insane, impossible]
-
-    $loop[$arrayLength[modes];
-            $let[i;$sub[$env[i];1]]
-            $if[$or[$get[i]==0;$get[i]==3];$addActionRow] 
-
-            $if[$getUserVar[rtMode]==$arrayAt[modes;$get[i]];
-                    $addButton[$arrayAt[modes;$get[i]]-$authorID;$toTitleCase[$arrayAt[modes;$get[i]]];Secondary;;true]
-            ;
-                    $addButton[$arrayAt[modes;$get[i]]-$authorID;$toTitleCase[$arrayAt[modes;$get[i]]];Success]
-            ];i;desc]
-    
-`
+  code: embed()
 },{
   type: "interactionCreate",
   allowedInteractionTypes: [ "button" ],
   code: `
     $textSplit[$customID;-]
-    $onlyIf[$splitText[1]==$authorID]
+    $onlyIf[$splitText[1]==$authorID;  $sendMessage[$channelID;$ephemeral This button is not for you!]  ]
     $onlyIf[$or[$splitText[0]==inferno;$splitText[0]==default;$splitText[0]==medium;$splitText[0]==hard;$splitText[0]==insane;$splitText[0]==impossible]]
 
-    $log[$splitText[0]]
+    $setUserVar[rtMode;$splitText[0]]
+
+    ${embed()}
 
     $deferUpdate`
 }]
+
+function embed() {
+    return `
+        $author[$userDisplayName • MUID: $getUserVar[MUID];$userAvatar]
+        $description[# Choose your raretry mode:]
+        $color[228b22]
+
+        $arrayLoad[modes;, ;inferno, default, medium, hard, insane, impossible]
+
+        $loop[$arrayLength[modes];  $let[i;$sub[$env[i];1]]  $if[$or[$get[i]==0;$get[i]==3];$addActionRow] 
+
+            $if[$getUserVar[rtMode]==$arrayAt[modes;$get[i]];
+                    $addButton[$arrayAt[modes;$get[i]]-$authorID;$toTitleCase[$arrayAt[modes;$get[i]]];Secondary;;true]
+            ;
+                    $addButton[$arrayAt[modes;$get[i]]-$authorID;$toTitleCase[$arrayAt[modes;$get[i]]];Success]
+            ]
+
+        ;i;desc] `
+}
