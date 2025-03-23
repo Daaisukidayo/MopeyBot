84c5f34cb443b6b6c8775318dd51db5313d6375b
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-23 19:34:27 +0100

    add reset-coins command with confirmation buttons for coin deletion

---
+++ commands/special/reset_coins.js
@@ -0,0 +1,27 @@
+module.exports = [{ name: "reset-coins", aliases: ["rc", "rmc", "reset-my-coins"], type: "messageCreate", code: `
+
+$reply
+$onlyIf[$getUserVar[MC]>0;You don't have any $getGlobalVar[emoji] to delete!]
+
+Are you sure you want to delete all your \`$separateNumber[$getUserVar[MC];,]\`$getGlobalVar[emoji]? 
+
+$addActionRow
+$addButton[confirm_deleting_coins-$authorID;Confirm;Success]
+$addButton[decline_deleting_coins-$authorID;Decline;Danger]
+
+`},
+
+{ type: "interactionCreate", allowedInteractionTypes: [ "button" ], code:`
+$textSplit[$customID;-]
+$onlyIf[$splitText[1]==$authorID;  $ephemeral $interactionReply[This button is not for you!]  ]
+$onlyIf[$or[$splitText[0]==confirm_deleting_coins;$splitText[0]==decline_deleting_coins]]
+
+
+$if[$splitText[0]==confirm_deleting_coins;
+    $!editMessage[$channelID;$messageID;Deleted all your \`$separateNumber[$getUserVar[MC];,]\`$getGlobalVar[emoji]!]
+    $deleteUserVar[MC;$authorID]
+;
+    $!editMessage[$channelID;$messageID;Delition declined!]
+]
+
+`}]
