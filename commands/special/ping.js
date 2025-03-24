7258396bc8e21e173b2e4834c728316d20ae3a68
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-24 17:06:38 +0100

    Update ping.js

--- commands/special/ping.js
+++ commands/special/ping.js
@@ -11,6 +11,8 @@ module.exports = ({
     
     $let[cdTime;5s]
     $if[$getUserVar[dev]==false; $userCooldown[$commandName;$get[cdTime];$callFunction[cooldownSchema;$commandName]] ]
-    **Current bot ping: \`$ping\`ms**\n**Execution time: \`$floor[$executionTime]\`ms** 
+
+    $if[$getUserVar[l10n]==EN;  **Current bot ping: \`$ping\`ms**\n**Execution time: \`$floor[$executionTime]\`ms**  ;
+    $if[$getUserVar[l10n]==RU;  **Текущая задержка бота: \`$ping\`мс**\n**Время выполнения: \`$floor[$executionTime]\`мс**  ]]
   `
 })
