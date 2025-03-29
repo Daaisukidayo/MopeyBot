98c167f26b7460023dfd836faa63f42f23787e5a
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-29 21:58:51 +0100

    feat: enhance invite command structure and formatting for better readability

--- commands/special/invite.js
+++ commands/special/invite.js
@@ -1,15 +1,19 @@
-module.exports = [{ name: "invite", aliases: ["inv"], type: "messageCreate", code: `
-$reply
+module.exports = [{
+  name: "invite",
+  aliases: ["inv"],
+  type: "messageCreate",
+  code: `
+    $reply
 
-$let[cdTime;5s]
-$callFunction[checking;]
-$callFunction[cooldown;$get[cdTime]]
+    $let[cdTime;5s]
+    $callFunction[checking;]
+    $callFunction[cooldown;$get[cdTime]]
 
-$title[Invite links]
-$description[## $hyperlink[Invite URL;https://discord.com/oauth2/authorize?client_id=693088307285983254]
-## $hyperlink[Official Discord Server;https://discord.gg/FE4dqGD]
-## $hyperlink[Top.gg;https://top.gg/bot/693088307285983254]]
-$color[ffd700]
-$author[$userDisplayName • MUID: $getUserVar[MUID];$userAvatar]
-
-`}]
+    $title[Invite links]
+    $description[## $hyperlink[Invite URL;https://discord.com/oauth2/authorize?client_id=693088307285983254]
+    ## $hyperlink[Official Discord Server;https://discord.gg/FE4dqGD]
+    ## $hyperlink[Top.gg;https://top.gg/bot/693088307285983254]]
+    $color[ffd700]
+    $author[$userDisplayName • MUID: $getUserVar[MUID];$userAvatar]
+  `
+}];
