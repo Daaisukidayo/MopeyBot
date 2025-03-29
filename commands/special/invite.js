1115064172502ba27f1e8af06c6dcc226de35e03
daisukidayo <gromovdmitrij2004@gmail.com>
2025-03-29 20:36:21 +0100

    feat: update invite command to enhance link presentation with titles and descriptions

--- commands/special/invite.js
+++ commands/special/invite.js
@@ -5,10 +5,10 @@ $let[cdTime;5s]
 $callFunction[checking;]
 $callFunction[cooldown;$get[cdTime]]
 
-
-$addField[__Invite URL__;[Click\](https://discord.com/oauth2/authorize?client_id=693088307285983254)]
-$addField[__Official Discord Server__;[Click\](https://discord.gg/FE4dqGD)]
-$addField[__Top.gg__;[Click\](https://top.gg/bot/693088307285983254)]
+$title[Invite links]
+$description[## $hyperlink[Invite URL;https://discord.com/oauth2/authorize?client_id=693088307285983254]
+## $hyperlink[Official Discord Server;https://discord.gg/FE4dqGD]
+## $hyperlink[Top.gg;https://top.gg/bot/693088307285983254]]
 $color[ffd700]
 $author[$userDisplayName • MUID: $getUserVar[MUID];$userAvatar]
 
