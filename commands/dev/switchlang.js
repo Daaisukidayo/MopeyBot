1a3b307e8539fba8397c20d1c56514a92044380d
daisukidayo <gromovdmitrij2004@gmail.com>
2025-04-14 20:30:24 +0200

    fix

--- commands/dev/switchlang.js
+++ commands/dev/switchlang.js
@@ -3,10 +3,12 @@ module.exports = [{
   type: "messageCreate",
   code: `
     $reply
+    $onlyIf[$authorID==$botOwnerID]
+
     $if[$getUserVar[l10n]==EN; 
-        $setUserVar[l10n;RU] ## Switched your localization language to \`$getUserVar[l10n]\`!
+        $setUserVar[l10n;RU] ## Switched your localization to \`$getUserVar[l10n]\`!
     ;
-        $setUserVar[l10n;EN] ## Switched your localization language to \`$getUserVar[l10n]\`!
+        $setUserVar[l10n;EN] ## Switched your localization to \`$getUserVar[l10n]\`!
     ]
   `
 }];
