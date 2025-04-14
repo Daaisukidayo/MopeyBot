0c57a62103603c92caad575898cc14f2f2eaeb0c
daisukidayo <gromovdmitrij2004@gmail.com>
2025-04-14 18:39:58 +0200

    added localization handling

--- commands/special/ping.js
+++ commands/special/ping.js
@@ -6,7 +6,18 @@ module.exports = ({
     
     $callFunction[checking;]
 
-    $if[$getUserVar[l10n]==EN;  **Current bot ping: \`$ping\`ms**\n**Execution time: \`$floor[$executionTime]\`ms**  ;
-    $if[$getUserVar[l10n]==RU;  **Текущая задержка бота: \`$ping\`мс**\n**Время выполнения: \`$floor[$executionTime]\`мс**  ]]
+    $jsonLoad[l10n;$readFile[json/localisations.json]]
+
+    $let[l10n;$getUserVar[l10n]]
+
+    $let[desc1;$env[l10n;ping;pingDesc1;$get[l10n]]]
+    $let[desc2;$env[l10n;ping;pingDesc2;$get[l10n]]]
+    $let[descMS;$env[l10n;ping;pingMS;$get[l10n]]]
+
+    $if[$get[desc1]==; $let[desc1;textNotFound]]
+    $if[$get[desc2]==; $let[desc2;textNotFound]]
+    $if[$get[descMS]==; $let[descMS;textNotFound]]
+
+    **$get[desc1]: \`$ping\`$get[descMS]**\n**$get[desc2]: \`$floor[$executionTime]\`$get[descMS]** 
   `
 })
