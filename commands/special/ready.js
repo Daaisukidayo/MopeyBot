c86117ec21208e4e2436eee3db90b686609f38b9
daisukidayo <gromovdmitrij2004@gmail.com>
2025-06-19 16:15:40 +0200

    fix

--- commands/special/ready.js
+++ commands/special/ready.js
@@ -1,20 +1,15 @@
 module.exports = {
   type: "ready",
   code: `
-    $async[
-      $deletevars[page;;message]
-      $deletevars[crpage;;message]
-      $deletevars[pages;;message]
-      $deletevars[rowsPerPage;;message]
-      $logger[Unused message variables have been cleared!]
-    ]
-    
-    $async[$logger[Bot $username[$botID] is ready!]]
+    $deletevars[page;;message]
+    $deletevars[crpage;;message]
+    $deletevars[pages;;message]
+    $deletevars[rowsPerPage;;message]
+    $logger[Unused message variables have been cleared!]
+    $logger[Bot $username[$botID] is ready!]
 
     $setInterval[
       $setStatus[online;Watching;$guildCount servers]
     ;12s]
-
-    
   `
 }
