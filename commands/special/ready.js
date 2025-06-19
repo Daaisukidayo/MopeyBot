b1d5e3e5e0498a3e93613c029d44c211f8cecb54
daisukidayo <gromovdmitrij2004@gmail.com>
2025-06-19 16:19:34 +0200

    final fix

--- commands/special/ready.js
+++ commands/special/ready.js
@@ -5,11 +5,12 @@ module.exports = {
     $deletevars[crpage;;message]
     $deletevars[pages;;message]
     $deletevars[rowsPerPage;;message]
-    $logger[Info;Unused message variables have been cleared!]
+
     $logger[Info;Bot $username[$botID] is ready!]
 
     $setInterval[
       $setStatus[online;Watching;$guildCount servers]
     ;12s]
+    
   `
 }
