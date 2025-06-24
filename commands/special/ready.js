978ff11eda830613c7315f9c2cf1281cff402070
daisukidayo <gromovdmitrij2004@gmail.com>
2025-06-24 19:06:49 +0200

    edit

--- commands/special/ready.js
+++ commands/special/ready.js
@@ -1,13 +1,7 @@
 module.exports = {
   type: "ready",
   code: `
-    $deletevars[page;;message]
-    $deletevars[crpage;;message]
-    $deletevars[pages;;message]
-    $deletevars[rowsPerPage;;message]
-
     $logger[Info;Bot $username[$botID] is ready!]
-
     $setInterval[
       $setStatus[online;Watching;$guildCount servers]
     ;12s]
