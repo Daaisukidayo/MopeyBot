4d70c102073f7008210aea9da31e5bf8b3868bdf
daisukidayo <gromovdmitrij2004@gmail.com>
2025-06-29 20:01:30 +0200

    edit

--- commands/luck/raretryMode.js
+++ commands/luck/raretryMode.js
@@ -11,29 +11,22 @@ module.exports = [{
     $callFunction[cooldown;${CD}]
 
     $if[$message!=;
-      $let[arg;$toLowerCase[$message[0]]]
+      $let[arg;"$toLowerCase[$message[0]]"]
 
-      $if[$or[$get[arg]==inferno;$get[arg]==inf;$get[arg]==1];
-        $!jsonSet[userProfile;rtMode;inferno]
-      ;$if[$or[$get[arg]==default;$get[arg]==def;$get[arg]==2];
-        $!jsonSet[userProfile;rtMode;default]
-      ;$if[$or[$get[arg]==medium;$get[arg]==med;$get[arg]==3];
-        $!jsonSet[userProfile;rtMode;medium]
-      ;$if[$or[$get[arg]==hard;$get[arg]==4];
-        $!jsonSet[userProfile;rtMode;hard]
-      ;$if[$or[$get[arg]==insane;$get[arg]==ins;$get[arg]==5];
-        $!jsonSet[userProfile;rtMode;insane]
-      ;$if[$or[$get[arg]==impossible;$get[arg]==imp;$get[arg]==6];
-        $!jsonSet[userProfile;rtMode;impossible]
+      $if[$includes[$get[arg];"inferno";"inf";1];         $!jsonSet[userProfile;rtMode;inferno]
+      ;$if[$includes[$get[arg];"default";"def";2];        $!jsonSet[userProfile;rtMode;default]
+      ;$if[$includes[$get[arg];"medium";"med";3];         $!jsonSet[userProfile;rtMode;medium]
+      ;$if[$includes[$get[arg];"hard";4];                 $!jsonSet[userProfile;rtMode;hard]
+      ;$if[$includes[$get[arg];"insane";"ins";5];         $!jsonSet[userProfile;rtMode;insane]
+      ;$if[$includes[$get[arg];"impossible";"imp";6];     $!jsonSet[userProfile;rtMode;impossible]
       ]]]]]]
     ]
 
-
     ${jsonAndArray()}
-    $let[msgid;$sendMessage[$channelID;${embed()};true]]
-    $setUserVar[userProfile;$env[userProfile]]
+    ${embed()}
     ${buttonsLoop()}
-    $!editMessage[$channelID;$get[msgid];${embed()}]
+    $let[msgid;$sendMessage[$channelID;;true]]
+    $setUserVar[userProfile;$env[userProfile]]
 
     ${timeout()}
   `
@@ -41,21 +34,21 @@ module.exports = [{
   type: "interactionCreate",
   allowedInteractionTypes: [ "button" ],
   code: `
-    $textSplit[$customID;-]
+    $arrayLoad[btn;-;$selectMenuValues]
+    $onlyIf[$includes[$env[btn;0];inferno;default;medium;hard;insane;impossible]]
+    $onlyIf[$includes[$env[btn;1];$authorID];$callFunction[notYourBTN]]
 
-    $onlyIf[$splitText[1]==$authorID;  $callFunction[notYourBTN;]  ]
-    $onlyIf[$or[$splitText[0]==inferno;$splitText[0]==default;$splitText[0]==medium;$splitText[0]==hard;$splitText[0]==insane;$splitText[0]==impossible]]
     $jsonLoad[userProfile;$getUserVar[userProfile]]
-
-    $!jsonSet[userProfile;rtMode;$splitText[0]]
+    $!jsonSet[userProfile;rtMode;$env[btn;0]]
+    $setUserVar[userProfile;$env[userProfile]]
     $let[msgid;$messageID]
 
     ${jsonAndArray()}
-    $setUserVar[userProfile;$env[userProfile]]
     ${buttonsLoop()}
     $!editMessage[$channelID;$get[msgid];${embed()}]
 
     $deferUpdate
+
     $!clearTimeout[RTM-$authorID]
     ${timeout()}
   `
@@ -76,20 +69,18 @@ function embed() {
 `
 }
 
-function buttonsLoop(disable = false){
+function buttonsLoop(){
   return `
-    $loop[$arrayLength[raretryModes];
-      $let[i;$sub[$env[i];1]]
-      $if[$or[$get[i]==0;$get[i]==3];
+    $let[q;0]
+    $arrayForEach[raretryModes;mode;
+      $if[$math[$get[q] % 3]==0;
         $addActionRow
-      ] 
-
-      $if[$env[userProfile;rtMode]==$toLowerCase[$arrayAt[raretryModes;$get[i]]];
-        $addButton[$toLowerCase[$arrayAt[raretryModes;$get[i]]]-$authorID;$arrayAt[raretryModes;$get[i]];Secondary;;true]
-      ;
-        $addButton[$toLowerCase[$arrayAt[raretryModes;$get[i]]]-$authorID;$arrayAt[raretryModes;$get[i]];Success;;${disable}]
       ]
 
+      $let[dis;$checkCondition[$env[userProfile;rtMode]==$toLowerCase[$env[mode]]]]
+
+      $addButton[$toLowerCase[$env[mode]]-$authorID;$env[mode];Success;;$get[dis]]
+      $letSum[q;1]
     ;i;desc]
   `
 }
@@ -97,8 +88,7 @@ function buttonsLoop(disable = false){
 function timeout() {
   return `
     $setTimeout[ 
-      ${buttonsLoop(`true`)}
-      $!editMessage[$channelID;$get[msgid];${embed()} $color[GRAY] This message is now inactive. Run the command again.]
+      $!disableButtonsOf[$channelID;$get[msgid]]
     ;${CD};RTM-$authorID]
   `
 }
