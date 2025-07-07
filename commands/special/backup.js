97e5c2a2c00767bdc18ed928e85280785fef1253
daisukidayo <gromovdmitrij2004@gmail.com>
2025-07-07 18:25:30 +0200

    fix

--- commands/special/backup.js
+++ commands/special/backup.js
@@ -6,13 +6,22 @@ module.exports = [{
     $jsonLoad[userProfile;$getUserVar[userProfile]]
 		$jsonLoad[userBackup;$getUserVar[userBackup]]
 		$let[arg;$toLowerCase[$message]]
-		$onlyIf[$includes["$get[arg]";"load"];# If you want to load last save in case you lost your profile data, run \`$getGuildVar[prefix]backup load\`!]
-
-		$color[$getGlobalVar[defaultColor]]
-		$getGlobalVar[author]
+		$onlyIf[$includes["$get[arg]";"load"];
+      $description[# If you want to load your last save in case you lost your profile data, run \`$getGuildVar[prefix]backup load\`!]
+      $author[✖️ Error!]
+      $color[$getGlobalVar[errorColor]]
+		]
+    $onlyIf[$env[userBackup;MUID]!=;
+      $description[# You don't have any saved backup!]
+      $author[✖️ Error!]
+      $color[$getGlobalVar[errorColor]]
+    ]
 
 		$!jsonSet[userProfile;$env[userBackup]]
 		$setUserVar[userProfile;$env[userBackup]]
+
+		$color[$getGlobalVar[defaultColor]]
+		$getGlobalVar[author]
 		$description[## Successfully loaded last backup!]
   `
 }]
