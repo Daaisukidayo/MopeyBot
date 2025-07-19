c25bbbba0e325897578029db94e7ac73dab934ac
daisukidayo <gromovdmitrij2004@gmail.com>
2025-07-19 18:46:40 +0200

    upd

--- commands/luck/sim.js
+++ commands/luck/sim.js
@@ -10,333 +10,151 @@ module.exports = [{
     $callFunction[cooldown;$get[cdTime]]
 
     $jsonLoad[animals;$readFile[json/animals.json]]
-    $jsonLoad[animalsKeys;$jsonKeys[animals]]
-    $jsonLoad[raresData;${raresData()}]
     $jsonLoad[raresMap;$getGlobalVar[raresMap]]
-    $jsonLoad[total;{}]
-    $arrayLoad[limitAnimals;,;Markhor,Keel-Billed Toucan,Choco Toucan]
+    $jsonLoad[result;{}]
+    ${luckGenerator()}
 
-    $loop[140;
-      $arrayForEach[raresData;rare;
-        $let[anChance;$env[rare;chance]]
-        $let[att;$env[rare;atts]]
-        $let[rChance;$randomNumber[1;$math[$get[att] + 1]]]
-
-        $if[$get[anChance]>=$get[rChance];
-          $let[name;$env[rare;name]]
-          $if[$env[total;$get[name]]==;
-            $!jsonSet[total;$get[name];1]
-          ;
-            $!jsonSet[total;$get[name];$math[$env[total;$get[name]] + 1]]
-            $arrayForEach[limitAnimals;animal;
-              $if[$env[total;$env[animal]]>3;
-                $!jsonSet[total;$env[animal];3]
-              ]
-            ]
-          ]
-        ]
-      ]
-    ]
     $let[totalRares;0]
     $let[totalPoints;0]
     $let[msgdesc;]
 
-    $jsonLoad[fullNameMap;{}]
-    $arrayForEach[animalsKeys;animal;
-      $jsonSet[fullNameMap;$env[animals;$env[animal];fullName];$env[animal]]
-    ]
+    $jsonLoad[allRareAttemptsInfoEntries;$jsonEntries[allRareAttemptsInfo]]
 
-    $jsonLoad[totalEntr;$jsonEntries[total]]
-
-    $arrayForEach[totalEntr;entry;
-      $let[animal;$env[fullNameMap;$env[entry;0]]]
-      $let[rareEmoji;${addEmoji(`$get[animal]`)}]
-      $let[msgdesc;$get[msgdesc] $get[rareEmoji]\`$env[entry;1]\`]
-      $letSum[totalRares;$env[entry;1]]
-
-      $arrayForEach[raresMap;obj;
-        $jsonLoad[raresInCategory;$env[obj;rares]]
-        $if[$arrayIncludes[raresInCategory;$tolowercase[$get[animal]]];
-          $letSum[totalPoints;$math[$env[obj;points] * $env[entry;1]]]
+    $arrayForEach[allRareAttemptsInfoEntries;entry;
+      $jsonLoad[raresArr;$env[entry;1;attempts]]
+      $arrayForEach[raresArr;rare;
+        $if[$env[animals;$env[rare];isRare];
+          $let[quantity;$env[result;$env[rare]]]
+          $!jsonSet[result;$env[rare];$math[$get[quantity] + 1]]
+          $if[$and[${isCommon("$env[rare]")};$get[quantity]>3];;
+            $letsum[totalRares;1]
+          ]
         ]
       ]
     ]
 
-    $color[$getGlobalVar[luckyColor]]
-    $description[# $get[msgdesc]]
-    $footer[Total Points: $get[totalPoints] • TotalRares: $get[totalRares]]
-    $getGlobalVar[author]
+    $jsonLoad[result;$jsonEntries[result]]
 
-`
+    $arrayForEach[result;arr;
+      $let[animalID;$env[arr;0]]
+      $let[quantity;$env[arr;1]]
+      $let[type;name] $c[Change this to "emoji"]
+      $let[animal;$env[animals;$get[animalID];variants;0;$get[type]]]
+
+      $if[$and[${isCommon("$get[animalID]")};$get[quantity]>3];
+        $let[quantity;3]
+      ]
+
+      $let[msgdesc;$get[msgdesc]# $get[animal]\`$get[quantity]\`\n]
+      ${findingRareInRaresMapBase()}
+      $letsum[totalPoints;$math[$get[quantity] * $env[rareMap;points]]]
+    ]
+
+    $color[$getGlobalVar[luckyColor]]
+    $description[$trim[$get[msgdesc]]]
+    $footer[Points: $get[totalPoints] • Rares: $get[totalRares]]
+    $getGlobalVar[author]
+  `
 }]
 
-function raresData () {
+function isCommon (rare) {
+  return `$includes[${rare};chocoToucan;keelBilledToucan;markhor]`
+}
+
+function luckGenerator () {
   return `
-    [
-      {
-        "chance": "10",
-        "atts": "700",
-        "emoji": "${addEmoji("jackass")}",
-        "name": "${addName("jackass")}",
-        "trig": "${addTrig("jackass")}"
-      },
-      {
-        "chance": "1",
-        "atts": "3000",
-        "emoji": "${addEmoji("rareMacaw")}",
-        "name": "${addName("rareMacaw")}",
-        "trig": "${addTrig("rareMacaw")}"
-      },
-      {
-        "chance": "300",
-        "atts": "3000",
-        "emoji": "${addEmoji("blueMacaw")}",
-        "name": "${addName("blueMacaw")}",
-        "trig": "${addTrig("blueMacaw")}"
-      },
-      {
-        "chance": "10",
-        "atts": "500",
-        "emoji": "${addEmoji("girabie")}",
-        "name": "${addName("girabie")}",
-        "trig": "${addTrig("girabie")}"
-      },
-      {
-        "chance": "25",
-        "atts": "500",
-        "emoji": "${addEmoji("momaffieFamily")}",
-        "name": "${addName("momaffieFamily")}",
-        "trig": "${addTrig("momaffieFamily")}"
-      },
-      {
-        "chance": "50",
-        "atts": "500",
-        "emoji": "${addEmoji("momaffie")}",
-        "name": "${addName("momaffie")}",
-        "trig": "${addTrig("momaffie")}"
-      },
-      {
-        "chance": "1",
-        "atts": "3000",
-        "emoji": "${addEmoji("rareToucan")}",
-        "name": "${addName("rareToucan")}",
-        "trig": "${addTrig("rareToucan")}"
-      },
-      {
-        "chance": "12",
-        "atts": "3000",
-        "emoji": "${addEmoji("lavaToucan")}",
-        "name": "${addName("lavaToucan")}",
-        "trig": "${addTrig("lavaToucan")}"
-      },
-      {
-        "chance": "300",
-        "atts": "3000",
-        "emoji": "${addEmoji("fieryToucan")}",
-        "name": "${addName("fieryToucan")}",
-        "trig": "${addTrig("fieryToucan")}"
-      },
-      {
-        "chance": "600",
-        "atts": "3000",
-        "emoji": "${addEmoji("keelBilledToucan")}",
-        "name": "${addName("keelBilledToucan")}",
-        "trig": "${addTrig("keelBilledToucan")}"
-      },
-      {
-        "chance": "900",
-        "atts": "3000",
-        "emoji": "${addEmoji("chocoToucan")}",
-        "name": "${addName("chocoToucan")}",
-        "trig": "${addTrig("chocoToucan")}"
-      },
-      {
-        "chance": "1",
-        "atts": "250",
-        "emoji": "${addEmoji("blackPanther")}",
-        "name": "${addName("blackPanther")}",
-        "trig": "${addTrig("blackPanther")}"
-      },
-      {
-        "chance": "4",
-        "atts": "250",
-        "emoji": "${addEmoji("leopard")}",
-        "name": "${addName("leopard")}",
-        "trig": "${addTrig("leopard")}"
-      },
-      {
-        "chance": "5",
-        "atts": "250",
-        "emoji": "${addEmoji("jaguar")}",
-        "name": "${addName("jaguar")}",
-        "trig": "${addTrig("jaguar")}"
-      },
-      {
-        "chance": "5",
-        "atts": "1000",
-        "emoji": "${addEmoji("demonPufferfish")}",
-        "name": "${addName("demonPufferfish")}",
-        "trig": "${addTrig("demonPufferfish")}"
-      },
-      {
-        "chance": "165",
-        "atts": "1000",
-        "emoji": "${addEmoji("yellowPufferfish")}",
-        "name": "${addName("yellowPufferfish")}",
-        "trig": "${addTrig("yellowPufferfish")}"
-      },
-      {
-        "chance": "15",
-        "atts": "250",
-        "emoji": "${addEmoji("whiteTiger")}",
-        "name": "${addName("whiteTiger")}",
-        "trig": "${addTrig("whiteTiger")}"
-      },
-      {
-        "chance": "1",
-        "atts": "1000",
-        "emoji": "${addEmoji("blackLion")}",
-        "name": "${addName("blackLion")}",
-        "trig": "${addTrig("blackLion")}"
-      },
-      {
-        "chance": "5",
-        "atts": "1000",
-        "emoji": "${addEmoji("whiteLion")}",
-        "name": "${addName("whiteLion")}",
-        "trig": "${addTrig("whiteLion")}"
-      },
-      {
-        "chance": "14",
-        "atts": "1000",
-        "emoji": "${addEmoji("blackManedLion")}",
-        "name": "${addName("blackManedLion")}",
-        "trig": "${addTrig("blackManedLion")}"
-      },
-      {
-        "chance": "1",
-        "atts": "1000",
-        "emoji": "${addEmoji("blackLioness")}",
-        "name": "${addName("blackLioness")}",
-        "trig": "${addTrig("blackLioness")}"
-      },
-      {
-        "chance": "5",
-        "atts": "1000",
-        "emoji": "${addEmoji("whiteLioness")}",
-        "name": "${addName("whiteLioness")}",
-        "trig": "${addTrig("whiteLioness")}"
-      },
-      {
-        "chance": "114",
-        "atts": "1000",
-        "emoji": "${addEmoji("lioness")}",
-        "name": "${addName("lioness")}",
-        "trig": "${addTrig("lioness")}"
-      },
-      {
-        "chance": "1",
-        "atts": "1000",
-        "emoji": "${addEmoji("blackLionCub")}",
-        "name": "${addName("blackLionCub")}",
-        "trig": "${addTrig("blackLionCub")}"
-      },
-      {
-        "chance": "5",
-        "atts": "1000",
-        "emoji": "${addEmoji("whiteLionCub")}",
-        "name": "${addName("whiteLionCub")}",
-        "trig": "${addTrig("whiteLionCub")}"
-      },
-      {
-        "chance": "54",
-        "atts": "1000",
-        "emoji": "${addEmoji("lionCub")}",
-        "name": "${addName("lionCub")}",
-        "trig": "${addTrig("lionCub")}"
-      },
-      {
-        "chance": "1",
-        "atts": "4000",
-        "emoji": "${addEmoji("shaheen")}",
-        "name": "${addName("shaheen")}",
-        "trig": "${addTrig("shaheen")}"
-      },
-      {
-        "chance": "50",
-        "atts": "4000",
-        "emoji": "${addEmoji("predator")}",
-        "name": "${addName("predator")}",
-        "trig": "${addTrig("predator")}"
-      },
-      {
-        "chance": "1",
-        "atts": "3000",
-        "emoji": "${addEmoji("rareVulture")}",
-        "name": "${addName("rareVulture")}",
-        "trig": "${addTrig("rareVulture")}"
-      },
-      {
-        "chance": "15",
-        "atts": "700",
-        "emoji": "${addEmoji("bigGoat")}",
-        "name": "${addName("bigGoat")}",
-        "trig": "${addTrig("bigGoat")}"
-      },
-      {
-        "chance": "600",
-        "atts": "700",
-        "emoji": "${addEmoji("markhor")}",
-        "name": "${addName("markhor")}",
-        "trig": "${addTrig("markhor")}"
-      },
-      {
-        "chance": "2",
-        "atts": "1000",
-        "emoji": "${addEmoji("blackRhino")}",
-        "name": "${addName("blackRhino")}",
-        "trig": "${addTrig("blackRhino")}"
-      },
-      {
-        "chance": "4",
-        "atts": "1000",
-        "emoji": "${addEmoji("whiteRhino")}",
-        "name": "${addName("whiteRhino")}",
-        "trig": "${addTrig("whiteRhino")}"
-      },
-      {
-        "chance": "1",
-        "atts": "5000",
-        "emoji": "${addEmoji("greaterSpottedEagle")}",
-        "name": "${addName("greaterSpottedEagle")}",
-        "trig": "${addTrig("greaterSpottedEagle")}"
-      },
-      {
-        "chance": "1",
-        "atts": "5000",
-        "emoji": "${addEmoji("harpyEagle")}",
-        "name": "${addName("harpyEagle")}",
-        "trig": "${addTrig("harpyEagle")}"
-      },
-      {
-        "chance": "28",
-        "atts": "5000",
-        "emoji": "${addEmoji("goldenEagle")}",
-        "name": "${addName("goldenEagle")}",
-        "trig": "${addTrig("goldenEagle")}"
-      }
-  \\]
+    $jsonLoad[allRareAttemptsInfo;{}]
+    $jsonLoad[rareGroups;
+      [
+        "pigeon|pigeon|whiteDove", 
+        "pig|pig|pinkyPig|stinkyPig",
+        "deer|deer|doe|marshDeer",
+        "reindeer|reindeer|muskDeer",
+        "swinehoe|swinehoe|goldenPheasant",
+        "donkey|donkey|jackass",
+        "macaw|macaw|blueMacaw|rareMacaw",
+        "giraffe|giraffe|momaffie|momaffieFamily|girabie",
+        "cheetah|cheetah|jaguar|leopard|blackPanther",
+        "toucan|toucan|chocoToucan|keelBilledToucan|fieryToucan|lavaToucan|rareToucan",
+        "pufferfish|pufferfish|yellowPufferfish|demonPufferfish",
+        "tiger|tiger|whiteTiger",
+        "lion|lion|lioness|lionCub|blackManedLion|whiteLionCub|whiteLioness|whiteLion|blackLionCub|blackLioness|blackLion",
+        "falcon|falcon|predator|shaheen",
+        "vulture|vulture|rareVulture",
+        "rhino|rhino|whiteRhino|blackRhino",
+        "baldEagle|baldEagle|goldenEagle|harpyEagle|greaterSpottedEagle",
+        "markhor|undefined|markhor|bigGoat",
+        "whiteGiraffe|undefined|whiteGiraffe|giraffeFamily"
+      \\]
+    ]
+
+    $arrayForEach[rareGroups;groupConfig;
+      $arrayLoad[groupParts;|;$env[groupConfig]]
+      $let[keyName;$env[groupParts;0]]
+      $let[commonAnimal;$env[groupParts;1]]
+      $let[tier;$env[animals;$get[keyName];tier]]
+      $let[totalAttempts;$env[animals;$env[groupParts;2];rarity;1]]
+
+      $let[totalRare;0]
+      $arrayCreate[rarePool;0]
+
+      $loop[$math[$arrayLength[groupParts] - 2];
+        $let[ri;$math[$env[ri] + 1]]
+        $let[rareAnimal;$env[groupParts;$get[ri]]]
+        $let[countRare;$env[animals;$get[rareAnimal];rarity;0]]
+
+        $if[$get[countRare]!=;
+          $arrayCreate[oneRareArr;$get[countRare]]
+          $arrayFill[oneRareArr;$get[rareAnimal]]
+          $arrayConcat[rarePool;rarePool;oneRareArr]
+          $letSum[totalRare;$get[countRare]]
+        ]
+      ;ri;desc]
+
+      $let[totalCommons;$math[$get[totalAttempts] - $get[totalRare]]]
+      $arrayCreate[commonArr;$get[totalCommons]]
+      $arrayFill[commonArr;$get[commonAnimal]]
+
+      $arrayConcat[fullAttemptArr;rarePool;commonArr]
+      $arrayShuffle[fullAttemptArr]
+
+      $if[$includes[-$get[keyName]-;-pigeon-;-pig-;-deer-;-reindeer-;-swinehoe-];
+        $jsonLoad[fullAttemptArr;$arraySplice[fullAttemptArr;0;1]]
+      ]
+      $if[$includes[-$get[keyName]-;-donkey-;-macaw-;-giraffe-;-cheetah-;-toucan-;-pufferfish-];
+        $jsonLoad[fullAttemptArr;$arraySplice[fullAttemptArr;0;120]]
+      ]
+      $if[$includes[-$get[keyName]-;-tiger-];
+        $jsonLoad[fullAttemptArr;$arraySplice[fullAttemptArr;0;60]]
+      ]
+      $if[$includes[-$get[keyName]-;-lion-;-falcon-;-vulture-];
+        $jsonLoad[fullAttemptArr;$arraySplice[fullAttemptArr;0;75]]
+      ]
+      $if[$includes[-$get[keyName]-;-rhino-;-baldEagle-;-markhor-];
+        $jsonLoad[fullAttemptArr;$arraySplice[fullAttemptArr;0;30]]
+      ]
+      $if[$includes[-$get[keyName]-;-whiteGiraffe-];
+        $jsonLoad[fullAttemptArr;$arraySplice[fullAttemptArr;0;5]]
+      ]
+
+      $!jsonSet[allRareAttemptsInfo;$get[keyName];{
+        "attempts": $env[fullAttemptArr]
+      }]
+    ]
   `
 }
 
-function addEmoji (name) {
-  return `$env[animals;${name};variants;$env[userProfile;userWardrobe;${name}];emoji]`
-}
+function findingRareInRaresMapBase () {
+  return `
+    $loop[$arrayLength[raresMap];
+      $let[j;$math[$env[j] - 1]]
 
-function addName (name) {
-  return `$env[animals;${name};fullName]`
-}
+      $jsonLoad[rareMap;$arrayAt[raresMap;$get[j]]]
+      $jsonLoad[raresList;$env[rareMap;rares]]
 
-function addTrig (name) {
-  return `$env[animals;${name};trig]`
+      $if[$arrayIncludes[raresList;$get[animalID]];
+        $break
+      ]
+    ;j;desc]
+  `
 }
