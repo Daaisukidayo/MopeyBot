3aad7a6d4d62e19686f47af98736fa73b0416d1f
daisukidayo <gromovdmitrij2004@gmail.com>
2025-07-26 16:09:48 +0200

    upd

--- commands/1hluck/chart.js
+++ commands/1hluck/chart.js
@@ -7,8 +7,13 @@ module.exports = [{
     $jsonLoad[userProfile;$getUserVar[userProfile]]
     $callFunction[checking]
     $callFunction[cooldown;$get[cdTime]]
-    $image[https://media.discordapp.net/attachments/701793335941136464/1398029031198163055/luckchart.png]
     $color[$getglobalvar[luckyColor]]
+    $image[https://media.discordapp.net/attachments/701793335941136464/1398029031198163055/luckchart.png]
     $timestamp[1753385040000]
+
+    $if[$message[0]==event;
+      $image[https://media.discordapp.net/attachments/701793335941136464/1398666514663739474/eventluckchart.png]
+      $timestamp[1753213440000]
+    ]
   `
 }]
