33f0f69af00bba87b86e89221df10239d352c9ac
daisukidayo <gromovdmitrij2004@gmail.com>
2025-10-11 17:12:25 +0200

    test

--- src/topgg/posted.js
+++ src/topgg/posted.js
@@ -1,7 +1,6 @@
 export default {
   type: "posted",
   code: `
-    $stop
     $logger[Info;Posted stats to Top.gg]
     $sendMessage[$getGlobalVar[logChannelID];
       $description[## Posted stats to Top.gg]
