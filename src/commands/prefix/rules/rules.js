fe43783d3796a9da9a5392e97c87afb95275eb89
daisukidayo <gromovdmitrij2004@gmail.com>
2026-02-04 23:46:41 +0100

    fix

--- src/commands/prefix/rules/rules.js
+++ src/commands/prefix/rules/rules.js
@@ -8,10 +8,10 @@ export default {
     $checkProfile
     $addCooldown[10s]
 
-    $description[# $hyperlink[$tl[ui.rules.information];https://github.com/Daaisukidayo/MopeyBot_readOnly/blob/main/README.md]
-    # $hyperlink[$tl[ui.rules.tos];https://github.com/Daaisukidayo/MopeyBot_readOnly/blob/main/Mopey's_TOS.md]
-    # $hyperlink[$tl[ui.rules.privacyPolicy];https://github.com/Daaisukidayo/MopeyBot_readOnly/blob/main/Mopey's_TOS.md#-privacy-policy]
-    # $hyperlink[$tl[ui.rules.rules];https://github.com/Daaisukidayo/MopeyBot_readOnly/blob/main/Rules.md]]
+    $description[# $hyperlink[$tl[ui.rules.information];https://github.com/Daaisukidayo/MopeyBot/blob/main/README.md]
+    # $hyperlink[$tl[ui.rules.tos];https://github.com/Daaisukidayo/MopeyBot/blob/main/Mopey's_TOS.md]
+    # $hyperlink[$tl[ui.rules.privacyPolicy];https://github.com/Daaisukidayo/MopeyBot/blob/main/Mopey's_TOS.md#-privacy-policy]
+    # $hyperlink[$tl[ui.rules.rules];https://github.com/Daaisukidayo/MopeyBot/blob/main/Rules.md]]
     $addEmbed[lucky]
   `
 }
